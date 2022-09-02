7c5971a0b157504ec54dcafb5896e5d164a31274
rbdeveloppement <rbdeveloppement@gmail.com>
2022-09-02 09:21:00 +0200

    titre (avec difficulté)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 
 import './App.css';
+import Home from './pages/HomeScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-      </header>
-    </div>
+  <div><Home></Home></div>
   );
 }
 
---
+++ src/pages/HomeScreen.jsx
@@ -0,0 +1,10 @@
+
+
+const Home = () => {
+    return (
+        <div>
+           <h1>La boutique de Rémi Bernier</h1> 
+        </div>
+    )
+}
+export default Home
