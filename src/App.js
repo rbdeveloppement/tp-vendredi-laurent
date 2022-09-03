6105d88a4aa636fe4649049eb150049983287434
rbdeveloppement <rbdeveloppement@gmail.com>
2022-09-03 16:01:37 +0200

    E4 next

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/HomeScreen";
-
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import CategoryCard from './Components/CategoryCard'
+
 
 function App() {
 
 const productdescriptif = [
   {title: "Femmes",description :"la mode pour elles", Image :'./assets/img/femmes.jpg'},
-  {title: "Hommes",description :"la mode pour lui", image :"./assets/img/hommes.jpg"},
-  {title: "Enfants",description :"la mode pour eux", image :"./assets/img/enfants.jpg"},
-  {title: "Bébés",description :"la mode pour eux aussi", image :"./assets/img/bebe.jpg"}
+  {title: "Hommes",description :"la mode pour lui", Image :"./assets/img/hommes.jpg"},
+  {title: "Enfants",description :"la mode pour eux", Image :"./assets/img/enfants.jpg"},
+  {title: "Bébés",description :"la mode pour eux aussi", Image :"./assets/img/bebe.jpg"}
 ];
 const category = productdescriptif.map((product) => 
   <div className="col-12 col-lg-3" key={product}>
